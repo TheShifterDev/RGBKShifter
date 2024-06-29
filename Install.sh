fb2497e0f8932318572c3a77433558af98f217aa
Matthew J B <Matthew.Ball.Dev@protonmail.com>
2024-06-29 20:48:59 +0100

    added frame of include copying

--- Install.sh
+++ Install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-
-bear -- g++ -pedantic -Wall -Wshadow `libpng-config --cflags --ldflags` main.cpp -o Built/rgbkShifter.bin
+# compiles and puts a symlink to prog in /usr/bin/
+bear -- g++ -pedantic -Wall -Wshadow `libpng-config --cflags --ldflags` Src/main.cpp -o Built/rgbkShifter.bin
 #sudo ln -f -s Built/RGBKShifter /usr/bin/rgbkShifter
+
+#copys rgbkShifters includes into /usr/include/rgbkShifter/
+#sudo mkdir /usr/include/rgbkShifter
+#sudo cp Include/* /usr/include/rgbkShifter/
