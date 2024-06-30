253fbc3af486f0734ed9d0eb4ad5007286f4d45d
Matthew J B <Matthew.Ball.Dev@protonmail.com>
2024-06-30 18:28:05 +0100

    made lower only effect letters

--- Src/MakeLowerCase.cpp
+++ Src/MakeLowerCase.cpp
@@ -4,9 +4,7 @@
 namespace MakeLowerCase {
 std::string Lower(std::string inp) {
 	for(uint32_t i = 0; i < inp.length(); i++) {
-		if(inp[i] < 91) {
-			inp[i] += 32;
-		}
+		if(inp[i] > 64 && inp[i] < 91) {inp[i] += 32;}
 	}
 	return inp;
 }
