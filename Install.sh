5ab5f3dea20d5c6758542dcaacef89cbcb686218
Matthew J B <Matthew.Ball.Dev@protonmail.com>
2024-06-30 20:14:58 +0100

    made flags more maliable

--- Install.sh
+++ Install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 PROGNAME="rgbkshifter"
+DEBGFLAG=" -O0 -ggdb"
+COMPFLAG=" -pedantic -Wall -Wshadow"
+LIBPNG=" `libpng-config --cflags --ldflags`"
 # compiles and puts a symlink to prog in /usr/bin/
-bear -- g++ -pedantic -Wall -Wshadow `libpng-config --cflags --ldflags` Src/main.cpp -o Built/$PROGNAME.bin
+bear -- g++ $DEBGFLAG $COMPFLAG $LIBPNG Src/main.cpp -o Built/$PROGNAME.bin
 sudo ln -f -s Built/$PROGNAME.bin /usr/bin/$PROGNAME
 
 #copys rgbkShifters includes into /usr/include/rgbkShifter/
