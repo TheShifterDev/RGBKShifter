2850bd934dff4b6b979e8c5446c3a21e77962bc3
Matthew J B <Matthew.Ball.Dev@protonmail.com>
2024-07-26 16:57:17 +0100

    fixed prog not linking correctly

--- Install.sh
+++ Install.sh
@@ -5,8 +5,11 @@ DEBGFLAG=" -O0 -ggdb"
 COMPFLAG=" -pedantic -Wall -Wshadow"
 LIBPNG=" `libpng-config --cflags --ldflags`"
 # compiles and puts a symlink to prog in /usr/bin/
-bear -- g++ $DEBGFLAG $COMPFLAG $LIBPNG Src/main.cpp -o Built/$PROGNAME.bin
-sudo ln -f -s Built/$PROGNAME.bin /usr/bin/$PROGNAME
+#bear -- g++ $DEBGFLAG $COMPFLAG $LIBPNG Src/main.cpp -o Built/$PROGNAME.bin
+g++ $DEBGFLAG $COMPFLAG $LIBPNG Src/main.cpp -o Built/$PROGNAME.bin
+
+#sudo ln -f -s /Built/$PROGNAME.bin /usr/bin/$PROGNAME
+sudo ln -f ./Built/$PROGNAME.bin /usr/bin/$PROGNAME
 
 #copys rgbkShifters includes into /usr/include/rgbkShifter/
 sudo rm -rf /usr/include/$PROGNAME
