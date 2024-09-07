f510a0f0432e13860ae9aef389a0610a6e537638
Matthew J B <Matthew.Ball.Dev@protonmail.com>
2024-09-07 23:15:40 +0100

    fixed merge conflict

--- Install.sh
+++ Install.sh
@@ -7,13 +7,9 @@ LIBPNG=" `libpng-config --cflags --ldflags`"
 #TODO: replace ^ with the output of that incase libpngconfig does not exist
 
 # IMPORTANT: only have 1 enable at a time or compiler will complain
-<<<<<<< ours
 U_PNGPP=true
-=======
-U_PNGPP=true 
 # NOTE: you may need to add links to path in your bashrc
 # "export PATH=$PATH:/usr/include/png++:/usr/include/libpng"
->>>>>>> theirs
 
 # compiles and puts a symlink to prog in /usr/bin/
 COMMAND="$DEBGFLAG $COMPFLAG"
@@ -27,14 +23,7 @@ COMMAND=$COMMAND" Src/main.cpp -o Built/$PROGNAME.bin"
 g++ $COMMAND
 sudo ln -f ./Built/$PROGNAME.bin /usr/bin/$PROGNAME
 
-<<<<<<< ours
 #copys rgbkShifters includes into /usr/include/StomaImagePack/
 sudo rm -rf /usr/include/StomaImagePack
 sudo mkdir /usr/include/StomaImagePack
 sudo cp Include/* /usr/include/StomaImagePack/
-=======
-#copys rgbkShifters includes into /usr/include/rgbkShifter/
-sudo rm -rf /usr/include/$PROGNAME
-sudo mkdir /usr/include/$PROGNAME
-sudo cp Include/* /usr/include/$PROGNAME/
->>>>>>> theirs
