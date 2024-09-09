a582b5c8f4db77757b4aa196e5ad5a175485871f
Matthew J B <Matthew.Ball.Dev@protonmail.com>
2024-09-09 09:54:46 +0100

    seperated StomaImagePack into own project

--- Install.sh
+++ Install.sh
@@ -4,26 +4,12 @@ PROGNAME="rgbkshifter"
 DEBGFLAG=" -O0 -ggdb"
 COMPFLAG=" -pedantic -Wall -Wshadow"
 LIBPNG=" `libpng-config --cflags --ldflags`"
-#TODO: replace ^ with the output of that incase libpngconfig does not exist
 
-# IMPORTANT: only have 1 enable at a time or compiler will complain
 U_PNGPP=true
-# NOTE: you may need to add links to path in your bashrc
-# "export PATH=$PATH:/usr/include/png++:/usr/include/libpng"
-
 # compiles and puts a symlink to prog in /usr/bin/
-COMMAND="$DEBGFLAG $COMPFLAG"
-######################
-if $U_PNGPP; then # requires png++ (c++ wrapper for libpng)
-COMMAND="$COMMAND $LIBPNG -DUSING_PNGPP"
-fi
+COMMAND="$DEBGFLAG $COMPFLAG $LIBPNG -DUSING_PNGPP"
 ######################
 COMMAND=$COMMAND" Src/main.cpp -o Built/$PROGNAME.bin"
 
-g++ $COMMAND
+bear -- g++ $COMMAND
 sudo ln -f ./Built/$PROGNAME.bin /usr/bin/$PROGNAME
-
-#copys rgbkShifters includes into /usr/include/StomaImagePack/
-sudo rm -rf /usr/include/StomaImagePack
-sudo mkdir /usr/include/StomaImagePack
-sudo cp Include/* /usr/include/StomaImagePack/
--- Src/main.cpp
+++ Src/main.cpp
@@ -1,7 +1,9 @@
 
-#include "../Include/StomaImagePack.hh"
+#include <StomaImagePack/StomaImagePack.hh>
 #define STOMAIMAGEPACK_IMPLEM
-#include "../Include/StomaImagePack.hh"
+#include <StomaImagePack/StomaImagePack.hh>
+
+#include <png++/png.hpp> // local install via pacman
 
 #include <string>
 
@@ -21,7 +23,27 @@ enum class FileType {
 	ENDOF
 };
 
+void Write_png(StomaImagePack::Image IMG, std::string NAM);
+StomaImagePack::Image Read_png(std::string NAM);
+void SliceOutLastOfChar(std::string INP,
+						char TARG,
+						std::string &OutStart,
+						std::string &OutEnd);
+std::string LowerCaseify(std::string INP);
 void WriteOut(StomaImagePack::Image IMG, std::string NAM);
+// NOTE: NEVER USE MACROS LIKE
+// >>> #define GETCORD(X, Y, XMAX) (X + (Y * XMAX)) <<<
+// AS NOT INCLUDING '('&')' IN
+// "GETCORD((MainXPos + SubXPos), (MainYPos + SubYPos),MainImage.Size.Width)"
+// CAUSED A BUG THAT TOOK DAYS TO TRACK
+inline uint32_t GetCoordinate(uint32_t XPOS, uint32_t YPOS, uint32_t MAXX);
+
+std::vector<StomaImagePack::Image> SeperateGlyphs(std::vector<StomaImagePack::Image> IMG);
+std::vector<StomaImagePack::Image> ReorderByVolume(std::vector<StomaImagePack::Image> IMG);
+float GetRGBColourDistance(StomaImagePack::Colour A, StomaImagePack::Colour B);
+void PalletiseImage(StomaImagePack::Image &IMG, std::vector<StomaImagePack::Colour> PAL);
+StomaImagePack::Image MergeImages(std::vector<StomaImagePack::Image> IMG);
+std::vector<StomaImagePack::Colour> ExtractPallet_Image(StomaImagePack::Image IMG);
 
 
 bool CommandList[(uint8_t)CommandEnum::ENDOF] = {};
@@ -131,12 +153,12 @@ int main(int argc, char *argv[]) {
 		StomaImagePack::Image TempImage;
 		for(uint32_t i = 0; i < FileList.size(); i++) {
 			// split "./gorbinos/file.png" to "./gorbinos/file" & "png"
-			StomaImagePack::SliceOutLastOfChar(FileList[i], '.', ImageName, ImageExtension);
+			SliceOutLastOfChar(FileList[i], '.', ImageName, ImageExtension);
 			if(ImageExtension == "png") {
-				TempImage = StomaImagePack::Read_png(ImageName);
+				TempImage = Read_png(ImageName);
 				ImageList.push_back(TempImage);
 			} else if(ImageExtension == "stimpac") {
-				TempImage = StomaImagePack::Read_stimpac(ImageName);
+				TempImage = StomaImagePack::ReadStimpac(ImageName);
 				ImageList.push_back(TempImage);
 			}
 		}
@@ -150,14 +172,14 @@ int main(int argc, char *argv[]) {
 			std::string PalletName;
 			std::string PalletExtension;
 			StomaImagePack::Image PalletImage;
-			StomaImagePack::SliceOutLastOfChar(PaletFile, '.', PalletName, PalletExtension);
+			SliceOutLastOfChar(PaletFile, '.', PalletName, PalletExtension);
 			if(PalletExtension == "png") {
-				PalletImage = StomaImagePack::Read_png(PalletName);
-				PalletColours = StomaImagePack::ExtractPallet_Image(PalletImage);
+				PalletImage = Read_png(PalletName);
+				PalletColours = ExtractPallet_Image(PalletImage);
 			}
 		}
 		for(uint32_t i = 0; i < ImageList.size(); i++) {
-			StomaImagePack::PalletiseImage(ImageList[i], PalletColours);
+			PalletiseImage(ImageList[i], PalletColours);
 		}
 	}
 
@@ -167,7 +189,7 @@ int main(int argc, char *argv[]) {
 		if(!CommandList[(uint8_t)CommandEnum::Format]) {
 			OutFileType = FileType::STIMPAC;
 		}
-		StomaImagePack::Image AtlasImage = StomaImagePack::MergeImages(ImageList);
+		StomaImagePack::Image AtlasImage = MergeImages(ImageList);
 		WriteOut(AtlasImage, OutDir + OutName);
 	} else
 	if(CommandList[(uint8_t)CommandEnum::Cutup]) {
@@ -176,7 +198,7 @@ int main(int argc, char *argv[]) {
 			OutFileType = FileType::PNG;
 		}
 		std::vector<StomaImagePack::Image> GlyphImageList =
-			StomaImagePack::SeperateGlyphs(ImageList);
+			SeperateGlyphs(ImageList);
 		for(uint32_t i = 0; i < GlyphImageList.size(); i++) {
 			WriteOut(GlyphImageList[i],
 					 OutDir + OutName + GlyphImageList[i].Glyphs[0].Name);
@@ -197,8 +219,339 @@ int main(int argc, char *argv[]) {
 
 void WriteOut(StomaImagePack::Image IMG, std::string NAM) {
 	if(OutFileType == FileType::PNG) {
-		StomaImagePack::Write_png(IMG, NAM);
+		Write_png(IMG, NAM);
 	} else {
-		StomaImagePack::Write_stimpac(IMG, NAM);
+		StomaImagePack::WriteStimpac(IMG, NAM);
 	}
 }
+
+StomaImagePack::Image Read_png(std::string NAM) {
+	// assumes .png has been trimmed off
+	uint32_t HoldPosition;
+	StomaImagePack::Image ReturnImage;
+	png::rgba_pixel HoldPixel;
+	std::string SourceDir;
+	std::string GlyphName;
+	png::image<png::rgba_pixel> PngFile(NAM + ".png");
+	ReturnImage.Size.Width = PngFile.get_width();
+	ReturnImage.Size.Height = PngFile.get_height();
+	// pngs only have 1 glyph
+	ReturnImage.Glyphs.resize(1);
+	SliceOutLastOfChar(NAM, '/',SourceDir,GlyphName);
+	ReturnImage.Glyphs[0].Name = GlyphName;
+	ReturnImage.Glyphs[0].Size = ReturnImage.Size;
+	// handle pixel array
+	ReturnImage.Pixels.resize(ReturnImage.Size.Width * ReturnImage.Size.Height);
+	for(uint32_t h = 0; h < ReturnImage.Size.Height; h++) {
+		for(uint32_t w = 0; w < ReturnImage.Size.Width; w++) {
+			HoldPosition = GetCoordinate(w, h, ReturnImage.Size.Width);
+			HoldPixel = PngFile.get_pixel(w, h);
+			ReturnImage.Pixels[HoldPosition].R = HoldPixel.red;
+			ReturnImage.Pixels[HoldPosition].G = HoldPixel.green;
+			ReturnImage.Pixels[HoldPosition].B = HoldPixel.blue;
+			ReturnImage.Pixels[HoldPosition].A = HoldPixel.alpha;
+		}
+	}
+	return ReturnImage;
+}
+void Write_png(StomaImagePack::Image IMG, std::string NAM) {
+	png::image<png::rgba_pixel> WriteFile;
+	WriteFile.resize(IMG.Size.Width, IMG.Size.Height);
+	png::rgba_pixel HoldPixel;
+	uint32_t CurrentCoordinate;
+	for(uint32_t h = 0; h < IMG.Size.Height; h++) {
+		for(uint32_t w = 0; w < IMG.Size.Width; w++) {
+			CurrentCoordinate = GetCoordinate(w, h, IMG.Size.Width);
+			HoldPixel.red = IMG.Pixels[CurrentCoordinate].R;
+			HoldPixel.green = IMG.Pixels[CurrentCoordinate].G;
+			HoldPixel.blue = IMG.Pixels[CurrentCoordinate].B;
+			HoldPixel.alpha = IMG.Pixels[CurrentCoordinate].A;
+			WriteFile.set_pixel(w, h, HoldPixel);
+		}
+	}
+	WriteFile.write(NAM + ".png");
+}
+
+void SliceOutLastOfChar(std::string INP, char TARG, std::string &OutStart,
+						std::string &OutEnd) {
+	uint32_t extenpos = 0;
+	std::string holdstr = INP;
+	extenpos = holdstr.size() - 1;
+	while(extenpos > 0) {
+		if(holdstr[extenpos] == TARG) {
+			break;
+		} else {
+			extenpos--;
+		}
+	}
+	if(extenpos == 0) {
+		OutStart = "";
+		OutEnd = holdstr;
+	} else {
+		OutStart = holdstr.substr(0, extenpos);
+		OutEnd = holdstr.substr(extenpos + 1, holdstr.size());
+	}
+}
+std::string LowerCaseify(std::string INP) {
+	for(uint32_t i = 0; i < INP.size(); i++) {
+		if(INP[i] > 64 && INP[i] < 91) {
+			INP[i] += 32;
+		}
+	}
+	return INP;
+}
+
+std::vector<StomaImagePack::Image> SeperateGlyphs(std::vector<StomaImagePack::Image> IMG) {
+	std::vector<StomaImagePack::Image> OutputImages;
+	StomaImagePack::Image HoldingImage;
+	StomaImagePack::Resolution NewSize;
+	StomaImagePack::Resolution NewOffset;
+	std::string NewName;
+	for(uint32_t i = 0; i < IMG.size(); i++) {
+		if(IMG[i].Glyphs.size() < 2) {
+			if(IMG[i].Glyphs.size() == 0){
+				// IMG[i] is broken
+				exit(8);
+			}
+			// skip as IMG[i] seperation is unnessesary
+			OutputImages.push_back(IMG[i]);
+		} else {
+			for(uint32_t q = 0; q < IMG[i].Glyphs.size(); q++) {
+				NewSize = IMG[i].Glyphs[q].Size;
+				NewOffset = IMG[i].Glyphs[q].Offset;
+				NewName =  IMG[i].Glyphs[q].Name;
+				HoldingImage.Size = NewSize;
+				HoldingImage.Glyphs.resize(1);
+				HoldingImage.Glyphs[0].Name = NewName;
+				HoldingImage.Glyphs[0].Offset.Width = 0;
+				HoldingImage.Glyphs[0].Offset.Height = 0;
+				HoldingImage.Glyphs[0].Size = NewSize;
+				HoldingImage.Pixels.resize(NewSize.Width * NewSize.Height);
+				for(uint32_t h = 0; h < NewSize.Height; h++) {
+				for(uint32_t w = 0; w < NewSize.Width; w++) {
+					HoldingImage.Pixels[GetCoordinate(w, h, NewSize.Width)] =
+						IMG[i].Pixels[GetCoordinate(NewOffset.Width + w,NewOffset.Height + h,IMG[i].Size.Width)];
+				}}
+				OutputImages.push_back(HoldingImage);
+			}
+		}
+	}
+
+	return OutputImages;
+}
+StomaImagePack::Image MergeImages(std::vector<StomaImagePack::Image> SUBIMAGES) {
+	// clang-format off
+	// ---------------------SET_SIZE------------------------------------//
+	StomaImagePack::Image OutputImage;
+	std::vector<StomaImagePack::Image> OrderedSubImages;
+	OrderedSubImages = ReorderByVolume(SUBIMAGES);
+	std::vector<StomaImagePack::Resolution> ImageResolutions(SUBIMAGES.size());
+	bool DrawSafe;
+	bool DrawDone;
+	uint32_t MPoint;
+	uint32_t SPoint;
+	uint32_t CheckLimitX;
+	uint32_t CheckLimitY;
+	uint32_t CurrentShape = 0;
+	// get total required volume
+	StomaImagePack::Resolution ExpectedSize;
+	ExpectedSize.Width = 0;
+	ExpectedSize.Height = 0;
+	StomaImagePack::Resolution MaxSize;
+	MaxSize.Width = 0;
+	MaxSize.Height = 0;
+
+	uint32_t CurrentGlyphs = 0;
+	uint32_t MaxGlyphs = 0;
+
+	for(uint32_t i = 0; i < OrderedSubImages.size(); i++) {
+		ImageResolutions[i] = OrderedSubImages[i].Size;
+		MaxSize.Width  += OrderedSubImages[i].Size.Width;
+		MaxSize.Height += OrderedSubImages[i].Size.Height;
+		MaxGlyphs += OrderedSubImages[i].Glyphs.size();
+	}
+	OutputImage.Glyphs.resize(MaxGlyphs);
+	// get base total size for new image
+	for (uint32_t i = 0;i<OrderedSubImages.size();i++){
+		// width
+		ExpectedSize.Width += OrderedSubImages[i].Size.Width;
+		if (ExpectedSize.Width>=(MaxSize.Width - ExpectedSize.Width)){break;}
+	}
+	for (uint32_t i = 0;i<OrderedSubImages.size();i++){
+		// height
+		ExpectedSize.Height += OrderedSubImages[i].Size.Height;
+		if (ExpectedSize.Height>=(MaxSize.Height - ExpectedSize.Height)){break;}		
+	}
+	
+	OutputImage.Size = ExpectedSize;
+	uint32_t hold = ExpectedSize.Width*ExpectedSize.Height;
+	OutputImage.Pixels.resize(hold);
+	std::vector<bool> OccupiedPositions(hold);
+	// ---------------------------DRAWING--------------------------------//
+	
+	// place shapes
+	for(CurrentShape = 0; CurrentShape < OrderedSubImages.size(); CurrentShape++) {
+		DrawDone = false;
+		CheckLimitY = (OutputImage.Size.Height-(OrderedSubImages[CurrentShape].Size.Height-1));
+		CheckLimitX = (OutputImage.Size.Width -(OrderedSubImages[CurrentShape].Size.Width -1));
+		for(uint32_t MYPos = 0; MYPos < CheckLimitY; MYPos++) {
+		for(uint32_t MXPos = 0; MXPos < CheckLimitX; MXPos++) {
+			if (OccupiedPositions[GetCoordinate(MXPos, MYPos, OutputImage.Size.Width)] == false) {
+				DrawSafe = true;
+				// check if position collides with previously written positions
+				for(uint32_t SYPos=0;SYPos<OrderedSubImages[CurrentShape].Size.Height;SYPos++){
+				for(uint32_t SXPos=0;SXPos<OrderedSubImages[CurrentShape].Size.Width ;SXPos++){
+					if(OccupiedPositions[GetCoordinate(MXPos + SXPos,MYPos + SYPos,OutputImage.Size.Width)] == true){
+						DrawSafe = false;
+						goto AlreadyWrittenBreakout;
+					}
+				}}
+				AlreadyWrittenBreakout:;
+				if (DrawSafe){
+					// draw
+					for(uint32_t SYPos=0;SYPos<OrderedSubImages[CurrentShape].Size.Height;SYPos++){
+					for(uint32_t SXPos=0;SXPos<OrderedSubImages[CurrentShape].Size.Width ;SXPos++){
+						MPoint = GetCoordinate(MXPos + SXPos,MYPos + SYPos,OutputImage.Size.Width);
+						SPoint = GetCoordinate(SXPos, SYPos, OrderedSubImages[CurrentShape].Size.Width);
+						OutputImage.Pixels[MPoint] = OrderedSubImages[CurrentShape].Pixels[SPoint];
+						OccupiedPositions[MPoint] = true;
+					}}
+					// place glyphs
+					uint32_t TargetGlyphID = OrderedSubImages[CurrentShape].Glyphs.size();
+					for(uint32_t CurrentGlyphID = 0;CurrentGlyphID < TargetGlyphID;CurrentGlyphID++){
+						OutputImage.Glyphs[CurrentGlyphs+CurrentGlyphID].Name 			= OrderedSubImages[CurrentShape].Glyphs[CurrentGlyphID].Name;
+						OutputImage.Glyphs[CurrentGlyphs+CurrentGlyphID].Offset.Width	= OrderedSubImages[CurrentShape].Glyphs[CurrentGlyphID].Offset.Width +MXPos;
+						OutputImage.Glyphs[CurrentGlyphs+CurrentGlyphID].Offset.Height	= OrderedSubImages[CurrentShape].Glyphs[CurrentGlyphID].Offset.Height+MYPos;
+						OutputImage.Glyphs[CurrentGlyphs+CurrentGlyphID].Size 			= OrderedSubImages[CurrentShape].Glyphs[CurrentGlyphID].Size;
+					}
+					CurrentGlyphs += TargetGlyphID;
+					DrawDone = true;
+					goto DrawDoneBreakout;
+				}
+			}// end of occupied position[offsetw,offseth] check
+		}}
+		DrawDoneBreakout:;
+		if(DrawDone == false){
+			exit(5);
+		}
+	}
+	// ---------------------------TRIM--------------------------------//
+	// Trim unused lines or collumns
+	ExpectedSize = OutputImage.Size;
+	// get true used Heigth/Rows
+	while (ExpectedSize.Height > 1) {
+		for(uint32_t w=0;w<ExpectedSize.Width;w++){
+			if (OccupiedPositions[GetCoordinate(w, ExpectedSize.Height-1, OutputImage.Size.Width)]){
+				goto PixelRowOccupiedBreakout;
+			}
+		}
+		ExpectedSize.Height--;
+	}
+	PixelRowOccupiedBreakout:;
+	// get true used Width/Columns
+	while (ExpectedSize.Width > 1) {
+		for(uint32_t h=0;h<ExpectedSize.Height;h++){
+			if (OccupiedPositions[GetCoordinate(ExpectedSize.Width-1 , h, OutputImage.Size.Width)]){
+				goto PixelColumnOccupiedBreakout;
+			}
+		}
+		ExpectedSize.Width--;
+	}
+	PixelColumnOccupiedBreakout:;
+	// check if there are rows or columns to trim
+	if(ExpectedSize.Width  != OutputImage.Size.Width 
+	|| ExpectedSize.Height != OutputImage.Size.Height){
+		StomaImagePack::Image CopyImage = OutputImage;
+		OutputImage.Size = ExpectedSize;
+		OutputImage.Pixels.resize(ExpectedSize.Width*ExpectedSize.Height);
+		for(uint32_t MYPos = 0; MYPos < CheckLimitY; MYPos++) {
+		for(uint32_t MXPos = 0; MXPos < CheckLimitX; MXPos++) {
+			MPoint = GetCoordinate(MXPos,MYPos,OutputImage.Size.Width);
+			SPoint = GetCoordinate(MXPos,MYPos,CopyImage.Size.Width);
+			OutputImage.Pixels[MPoint] = CopyImage.Pixels[SPoint];
+		}}
+	}
+	return OutputImage;
+	// clang-format on
+}
+float GetRGBColourDistance(StomaImagePack::Colour A, StomaImagePack::Colour B) {
+	return (((((float)A.R - (float)B.R) * ((float)A.R - (float)B.R))+
+			 (((float)A.G - (float)B.G) * ((float)A.G - (float)B.G))+
+			 (((float)A.B - (float)B.B) * ((float)A.B - (float)B.B))) / 2);
+}
+std::vector<StomaImagePack::Colour> ExtractPallet_Image(StomaImagePack::Image IMG) {
+	// brute force iterates over fed image
+	uint32_t t_len = IMG.Size.Width * IMG.Size.Height;
+	std::vector<StomaImagePack::Colour> t_ret;
+	bool t_add;
+	t_ret.push_back(IMG.Pixels[0]);
+	for(uint32_t i = 1; i < t_len; i++) {
+		t_add = true;
+		for(uint32_t q = 0; q < t_ret.size(); q++) {
+			if(t_ret[q].R == IMG.Pixels[i].R && t_ret[q].G == IMG.Pixels[i].G &&
+			   t_ret[q].B == IMG.Pixels[i].B) {
+				t_add = false;
+				break;
+			}
+		}
+		if(t_add) {
+			t_ret.push_back(IMG.Pixels[i]);
+		}
+	}
+	return t_ret;
+}
+void PalletiseImage(StomaImagePack::Image &IMG, std::vector<StomaImagePack::Colour> PAL) {
+	// iterate over every pixel
+	std::vector<float> distances;
+	distances.resize(PAL.size());
+	uint32_t ClosestPalletColour = 0;
+	uint32_t PixelCount = (IMG.Size.Width * IMG.Size.Height);
+	for(uint32_t i = 0; i < PixelCount; i++) {
+		// gets pixels distance to pallet colours (RGB only)
+		for(uint32_t q = 0; q < PAL.size(); q++) {
+			distances[q] = GetRGBColourDistance(PAL[q], IMG.Pixels[i]);
+		}
+		// gets closest pallet colour
+		ClosestPalletColour = 0;
+		for(uint32_t q = 1; q < PAL.size(); q++) {
+			if(distances[q] < distances[ClosestPalletColour]) {
+				ClosestPalletColour = q;
+			}
+		}
+		// make pixel its closest pallet counterpart
+		IMG.Pixels[i].R = PAL[ClosestPalletColour].R;
+		IMG.Pixels[i].G = PAL[ClosestPalletColour].G;
+		IMG.Pixels[i].B = PAL[ClosestPalletColour].B;
+	}
+}
+std::vector<StomaImagePack::Image> ReorderByVolume(std::vector<StomaImagePack::Image> UNORDERED) {
+	std::vector<uint32_t> volumest(UNORDERED.size());
+	uint32_t hold;
+	// create volumest list
+	for(uint32_t i = 0; i < UNORDERED.size(); i++) {
+		volumest[i] = i;
+	}
+	// reorder volumest
+	for(uint32_t i = 1; i < UNORDERED.size(); i++) {
+		if(UNORDERED[volumest[i - 1]].Pixels.size() <
+		   UNORDERED[volumest[i]].Pixels.size()) {
+			// if smaller switch and restart
+			hold = volumest[i];
+			volumest[i] = volumest[i - 1];
+			volumest[i - 1] = hold;
+			i = 0;
+		}
+	}
+	std::vector<StomaImagePack::Image> OrderedImages;
+	for(uint32_t i = 0; i < UNORDERED.size(); i++) {
+		OrderedImages.push_back(UNORDERED[volumest[i]]);
+	}
+	return OrderedImages;
+}
+
+inline uint32_t GetCoordinate(uint32_t XPOS, uint32_t YPOS, uint32_t MAXX) {
+	return (XPOS + (YPOS * MAXX));
+}
+
+
+
